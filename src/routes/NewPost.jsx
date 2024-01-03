c51cb926aeddd644a8bd5340f74a189f4ca6eb4b
Caio Bruno <caiobruno90@yahoo.com>
2024-01-03 17:45:34 -0300

    Requisição de post

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,8 +1,22 @@
-
+import "./NewPost.css"
 
 const NewPost = () => {
   return (
-    <div>NewPost</div>
+
+    <div className="new-post">
+      <h2>Inserir novo post</h2>
+      <form >
+        <div className="form-control">
+          <label htmlFor="title">Titulo:</label>
+          <input type="text" name="title" id="title" placeholder="Digite o rirulo" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Conteudo:</label>
+          <textarea type="text" name="body" id="body" placeholder="Digite o conteudo" />
+        </div>
+        <input type="submit" value="Criar post" className="btn" />
+      </form>
+    </div>
   )
 }
 
